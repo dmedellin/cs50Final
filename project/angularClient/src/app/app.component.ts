80cc32b40ad481914b5787394a351ac1971db443
dmedellin <davidjmedellin@gmail.com>
2022-07-15 23:31:04 -0400

    first cont down setted up

--- project/angularClient/src/app/app.component.ts
+++ project/angularClient/src/app/app.component.ts
@@ -9,25 +9,51 @@ declare var window: any;
 export class AppComponent {
   title = 'angularClient';
   rows: any[] = [];
-  tasks: any[] = [];
   goals: any[] = [];
+  tasks: any[] = [];
   displayName: string = '';
   formModal: any;
   offcanvas: any;
   selectedTaskLis: any;
+  sessionModal: any;
+  timeDisplay: any;
+
   constructor(private taskService: TaskServiceService) {
     this.loadTaskList();
 
   }
   ngAfterViewInit() {
     this.formModal = new window.bootstrap.Modal(
-      document.getElementById('exampleModal')
+      document.getElementById('createModal')
     );
     this.offcanvas = new window.bootstrap.Offcanvas(
       document.getElementById('offcanvasWithBothOptions')
     );
+    this.sessionModal = new window.bootstrap.Modal(
+      document.getElementById('sessionModal')
+    );
+  }
+  startSession() {
+    var duration = 60 * 25;
+    var timer = duration, minutes: number, seconds: number;
+    setInterval(() => {
+      minutes = parseInt((timer / 60).toString(), 10);
+      seconds = parseInt((timer % 60).toString(), 10);
+
+      var displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+      var displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+
+      this.timeDisplay = displayMinutes + ":" + displaySeconds;
+
+      if (--timer < 0) {
+        timer = duration;
+      }
+    }, 1000);
+    this.sessionModal.show();
   }
   loadTaskList() {
+    this.rows = [];
+    this.goals = [];
     this.taskService.getTasksList().subscribe(data => {
       this.goals = data as [];
       var row: any = [];
@@ -47,6 +73,7 @@ export class AppComponent {
   }
   openOffset(id: any) {
     this.selectedTaskLis = id;
+    this.tasks = [];
     this.taskService.getTasks(id).subscribe(data => {
       this.tasks = data as [];
     });
